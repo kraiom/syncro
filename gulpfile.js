6fd4e5294b060ad9fc04f451557b88a19822ed99
Breno Freitas <brenolimadefreitas@gmail.com>
2017-05-21 21:11:24 -0400

    Update build files

--- gulpfile.js
+++ gulpfile.js
@@ -1,101 +1,91 @@
-var gulp           = require('gulp'),
-    babelify       = require('babelify'),
-    browserify     = require('browserify'),
-    source         = require('vinyl-source-stream'),
-    bufferify      = require('vinyl-buffer'),
-    jshint         = require('gulp-jshint'),
-    uglify         = require('gulp-uglify'),
-    img            = require('gulp-imagemin'),
-    jscs           = require('gulp-jscs'),
-    argv           = require('yargs').argv,
-    gulpif         = require('gulp-if'),
-    connect        = require('gulp-connect'),
-    bower          = require('gulp-main-bower-files'),
-    flatten        = require('gulp-flatten'),
-    minifyCSS      = require('gulp-cssnano'),
-    prefixer       = require('gulp-autoprefixer')
+'use strict'
 
-var DEBUG = argv.production ? false : true
-var NAME = require('./package').name
+const $ = require('gulp-load-plugins')()
+
+const gulp = require('gulp')
+const babelify = require('babelify')
+const browserify = require('browserify')
+const source = require('vinyl-source-stream')
+const bufferify = require('vinyl-buffer')
+const argv = require('yargs').argv
+
+const DEBUG = argv.production ? false : true
+const NAME = require('./package').name
 
 gulp.task('default', ['scripts', 'html', 'img', 'audio', 'font', 'styles'])
 
-gulp.task('watch', ['default'], function () {
-    gulp.watch('./src/js/**/*', ['scripts'])
-    gulp.watch('./src/css/**/*', ['styles'])
-    gulp.watch('./src/html/**/*', ['html'])
-    gulp.watch('./src/json/**/*', ['json', 'scripts'])
+gulp.task('watch', ['default'], () => {
+  gulp.watch('./src/js/**/*', ['scripts'])
+  gulp.watch('./src/css/**/*', ['styles'])
+  gulp.watch('./src/html/**/*', ['html'])
+  gulp.watch('./src/json/**/*', ['json', 'scripts'])
 })
 
-gulp.task('code-check', function () {
-  return gulp
+gulp.task('code-check', () => gulp
   .src('./src/js/**/*')
-  .pipe(jshint())
-  .pipe(jshint.reporter('jshint-stylish'))
-  .pipe(jscs({configPath: './.jscsrc'}))
-})
+  .pipe($.jshint())
+  .pipe($.jshint.reporter('jshint-stylish'))
+  .pipe($.jscs({configPath: './.jscsrc'}))
+)
 
-gulp.task('scripts', ['json'], function () {
-    browserify({
-      entries: './src/js/states/main.js',
-      debug: DEBUG
-    })
-    .transform(babelify.configure({ stage: 0 }))
-    .bundle()
-    .pipe(source(NAME + '.min.js'))
-    .pipe(bufferify())
-    .pipe(gulpif(DEBUG === false, uglify()))
-    .pipe(gulp.dest('./build/assets/js/'))
+gulp.task('scripts', ['json'], () => {
+  browserify({
+    entries: './src/js/states/main.js',
+    debug: DEBUG
+  })
+  .transform(babelify.configure({ stage: 0 }))
+  .bundle()
+  .pipe(source(NAME + '.min.js'))
+  .pipe(bufferify())
+  .pipe($.if(!DEBUG, $.uglify()))
+  .pipe(gulp.dest('./build/assets/js/'))
 
-    return gulp
-    .src('./bower.json')
-    .pipe(bower())
-    .pipe(flatten())
-    .pipe(gulp.dest('./build/assets/libs/'))
-})
-
-gulp.task('html', function() {
   return gulp
+  .src('./bower.json')
+  .pipe($.mainBowerFiles())
+  .pipe($.flatten())
+  .pipe(gulp.dest('./build/assets/libs/'))
+})
+
+gulp.task('html', () => gulp
   .src('./src/html/**/*')
   .pipe(gulp.dest('./build/'))
-})
+)
 
-gulp.task ('styles', function () {
-  return gulp
+gulp.task ('styles', () => gulp
   .src('./src/css/*')
-  .pipe(prefixer())
-  .pipe(minifyCSS())
+  .pipe($.autoprefixer())
+  .pipe($.cssnano())
   .pipe(gulp.dest('./build/assets/css'))
-});
+)
 
-gulp.task('json', function() {
-  return gulp
+gulp.task('json', () => gulp
   .src('./src/json/*')
   .pipe(gulp.dest('./build/assets/json'))
-})
+)
 
-gulp.task('audio', function () {
-  return gulp
+gulp.task('audio', () => gulp
   .src('./src/audio/*')
   .pipe(gulp.dest('./build/assets/audio'))
-})
+)
 
-gulp.task('font', function () {
-  return gulp
+gulp.task('font', () => gulp
   .src('./src/fonts/*')
   .pipe(gulp.dest('./build/assets/fonts'))
-})
+)
 
-gulp.task('img', function() {
-  return gulp
+gulp.task('img', () => gulp
   .src('./src/img/*')
-  .pipe(img())
+  .pipe($.imagemin())
   .pipe(gulp.dest('./build/assets/img/'))
-})
+)
 
-gulp.task('serve', ['watch'], function () {
-  connect.server({
-    root: './build/',
-    livereload: true
-  })
-})
+gulp.task('serve', ['watch'], () => $.connect.server({
+  root: './build/',
+  livereload: true
+}))
+
+gulp.task ('deploy', ['default'], () => gulp
+  .src('./build/**/*')
+  .pipe($.ghPagesCname({ cname: 'syncro.kraiom.com' }))
+)
